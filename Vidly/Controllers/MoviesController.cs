eb9a37ccb9fd61a5f29f33382ca7b491a93f87ad
Mohamed Ramadan <rmadanfci20130197@gmail.com>
2018-02-13 04:06:56 +0200

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> Movies = new List<Movie>
+        {
+            new Movie {Id = 1, Name = "Fast & Feriou"},
+            new Movie {Id = 2, Name = "Xxx return of Xander Cage"}
+        };
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = Movies.Find(mov => mov.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly/Models/CustomersManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class CustomersManager
+    {
+        private static ApplicationDbContext _context;
+
+
+        public static IEnumerable<Customer> GetCustomers()
+        {
+            if (_context == null)
+                _context = new ApplicationDbContext();
+
+            return _context.Customers.Include(c => c.MembershipType);
+        }
+
+        public static List<Customer> GetCustomersAsList()
+        {
+            return GetCustomers().ToList();
+        }
+
+        public static Customer GetCustomerById(int id)
+        {
+            return GetCustomers().SingleOrDefault(c => c.Id == id);
+        }
+    }
+}
